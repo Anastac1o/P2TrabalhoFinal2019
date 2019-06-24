e77d4b6f713425b8a15d0a87e1194ed75e8b50d2
Anastac1o <32721335+Anastac1o@users.noreply.github.com>
2019-06-24 06:06:28 +0100

    Delete DrawForward.java

--- LSystems/src/DrawForward.java
+++
@@ -1,8 +0,0 @@
-public class DrawForward extends TurtleStatement{
-    double distance;
-    public DrawForward(double distance){ this.distance = distance;}
-    public double getDistance(){ return distance; }
-    public void run(Interpreter interpreter){
-        interpreter.runDrawForward(this);
-    }
-}
